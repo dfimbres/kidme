25cf1704fbee2d400bd40565bb4ade68f51d464a
David <dfimbres@users.noreply.github.com>
2022-02-05 01:23:06 -0700

    fleek ⚡️

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  reactStrictMode: true,
-  trailingSlash: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' }
+    reactStrictMode: true,
+    trailingSlash: true,
+    exportPathMap: function() {
+        return {
+            '/': { page: '/' }
+        }
     }
-  }
-}
+  };
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -13,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://rainbow.me/kidme.eth">kidme.eth</a>
+          Welcome to <a href="https://islands.xyz/@kidme/gallery">kidme.eth 🏝</a>
         </h1>
 
         <h1 className={styles.description}>Hello my name is David a Fullstack Web Developer</h1>
@@ -39,19 +38,6 @@ export default function Home() {
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
